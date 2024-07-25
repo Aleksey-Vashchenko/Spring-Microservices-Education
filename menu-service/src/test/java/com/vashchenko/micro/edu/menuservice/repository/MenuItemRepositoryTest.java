a176e4ccd305e26923df856463b29cc5ef33541d
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-07-26 02:39:24 +0300

    feat: create H2 test for myBatis mapper and flyway migration

--- menu-service/src/test/java/com/vashchenko/micro/edu/menuservice/repository/MenuItemRepositoryTest.java
+++ menu-service/src/test/java/com/vashchenko/micro/edu/menuservice/repository/MenuItemRepositoryTest.java
@@ -1,14 +1,26 @@
 package com.vashchenko.micro.edu.menuservice.repository;
 
+import com.vashchenko.micro.edu.menuservice.model.MenuItem;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@ActiveProfiles("test")
 class MenuItemRepositoryTest {
+
+    @Autowired
+    MenuItemRepository menuItemRepository;
 
     @Test
     void save() {
-
+        MenuItem item = Instancio.of(MenuItem.class).ignore(field(MenuItem::getId)).create();
+        menuItemRepository.save(item);
+        assertNotNull(item.getId());
     }
 
     @Test
