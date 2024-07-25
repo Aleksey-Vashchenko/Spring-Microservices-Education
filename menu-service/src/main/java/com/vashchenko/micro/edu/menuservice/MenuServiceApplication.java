d25232dbbef5054614b45c23c9dea08c00b8e5b7
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-07-26 00:22:54 +0300

    feat: initializing menu service

---
+++ menu-service/src/main/java/com/vashchenko/micro/edu/menuservice/MenuServiceApplication.java
@@ -0,0 +1,19 @@
+package com.vashchenko.micro.edu.menuservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServiceApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
