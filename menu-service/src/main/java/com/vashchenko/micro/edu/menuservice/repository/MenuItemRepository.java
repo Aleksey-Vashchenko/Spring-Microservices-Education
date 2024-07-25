a6e2c71fcd8237e876afb77865096daa39d2f030
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-07-26 02:35:22 +0300

    feat: update MenuItemRepository.java, menuItem-mapper.xml. Set locations for myBatis config and mappers

--- menu-service/src/main/java/com/vashchenko/micro/edu/menuservice/repository/MenuItemRepository.java
+++ menu-service/src/main/java/com/vashchenko/micro/edu/menuservice/repository/MenuItemRepository.java
@@ -1,9 +1,12 @@
 package com.vashchenko.micro.edu.menuservice.repository;
 
 import com.vashchenko.micro.edu.menuservice.model.MenuItem;
-
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
+@Mapper
 public interface MenuItemRepository {
     void save(MenuItem menuItem);
     List<MenuItem> getAll();
