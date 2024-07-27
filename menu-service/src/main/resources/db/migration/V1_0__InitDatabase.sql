cbdafc2f4e47794cf7758b34de2259e029492409
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-07-27 12:00:42 +0300

    menu-service: fix!: update V1_0__InitDatabase.sql

--- menu-service/src/main/resources/db/migration/V1_0__InitDatabase.sql
+++ menu-service/src/main/resources/db/migration/V1_0__InitDatabase.sql
@@ -1,7 +1,7 @@
 create table dishes(
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL UNIQUE,
-    name varchar,
-    price float,
+    name varchar not null,
+    price float not null,
     weight integer,
     description varchar
 )
